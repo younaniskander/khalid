301c23d216bfa83d0beb649402f00fabce9b9ba8
Younan Iskander <87044703+younaniskander@users.noreply.github.com>
2024-04-25 03:46:44 +0200

    app.py

--- app.py
+++ app.py
@@ -7,29 +7,6 @@ import numpy as np
 # Load the Keras model
 model = load_model('lungModel2.h5')
 
-def preprocess_image(image):
-    # Convert the image to a numpy array
-    image_array = tf.keras.preprocessing.image.img_to_array(image)
-    
-    # Expand the dimensions of the image to match the input shape of the model
-    image_array = tf.expand_dims(image_array, axis=0)
-    
-    # Define the parameters for data augmentation
-    datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        rotation_range=40,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest'
-    )
-    
-    # Apply data augmentation to the image
-    augmented_image = next(datagen.flow(image_array, batch_size=1))
-    
-    return augmented_image
-
 def main():
     st.title('Lung Cancer Detection')
     st.write('Upload an image of a lung for cancer detection')
@@ -42,11 +19,13 @@ def main():
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image', use_column_width=True)
 
-        # Preprocess the image
-        processed_image = preprocess_image(image)
+        # Resize and preprocess the image
+        resized_image = image.resize((IMG_SIZE, IMG_SIZE))
+        resized_image = np.array(resized_image) / 255.0
+        resized_image = resized_image.reshape(1, IMG_SIZE, IMG_SIZE, 3)
 
         # Make predictions
-        prediction = model.predict(processed_image)
+        prediction = model.predict(resized_image)
         classes = ['normal', 'adenocarcinoma', 'large.cell', 'squamous']
         predicted_class = classes[np.argmax(prediction)]
 
